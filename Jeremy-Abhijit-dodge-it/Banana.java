e4850cfc001df6b3967f01577eb8bf8c93aef91e
jeremythcheung <jeremythcheung@gmail.com>
2026-04-21 16:13:01 -0400

    Add Hero.java and Banana.java source files

---
+++ Jeremy-Abhijit-dodge-it/Banana.java
@@ -0,0 +1,32 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Banana extends Actor
+{
+    public void act()
+    {
+        move(-10);
+
+        if(getX() <= 0){
+            resetBanana();
+        }
+
+        if(isTouching(Hero.class))
+        {
+            Gameover gameover = new Gameover();
+            getWorld().addObject(gameover, 300, 200);
+            getWorld().removeObject(this);
+        }
+    }
+
+    public void resetBanana()
+    {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0){
+            setLocation(600, 100);
+        }
+        else
+        {
+            setLocation(600, 300);
+        }
+    }
+}
